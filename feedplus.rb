3a871fb803d01b69beb2e482c0ab8a909b922cc3
Andrea Scarpino <scarpino@kde.org>
2013-06-13 17:26:24 +0200

    Respect the max number of posts

--- feedplus.rb
+++ feedplus.rb
@@ -65,56 +65,63 @@ end
 uri = URI("https://www.googleapis.com/plus/v1/people/#{options.id}/activities/public")
 params = { :fields => 'items(actor/displayName,object(actor/displayName,attachments(content,displayName,fullImage/url),content),title,updated,url)',
            :key => 'AIzaSyDjcCZGSGTIaMA3VXmEjATkTlX4iRAoPiM',
-           :maxResults => options.limit }
+           :maxResults => 100 }
 uri.query = URI.encode_www_form(params)
 
 res = Net::HTTP.get_response(uri)
 if res.is_a?(Net::HTTPSuccess)
   items = JSON.parse(res.body)
 
-  rss = RSS::Maker.make("2.0") do |maker|
-    channel = maker.channel
-    channel.title = options.feedTitle
-    channel.description = channel.title
-    channel.link = options.feedUrl
+  catch :done do
+    rss = RSS::Maker.make("2.0") do |maker|
+      channel = maker.channel
+      channel.title = options.feedTitle
+      channel.description = channel.title
+      channel.link = options.feedUrl
 
-    items.fetch("items").each do |post|
-      content = post.fetch("object").fetch("content")
+      counter = 0
+      items.fetch("items").each do |post|
+        content = post.fetch("object").fetch("content")
 
-      catch :none do
-        options.hashtags.each do |tag|
-          throw :none unless content.downcase.include?("##{tag}")
-        end
+        catch :none do
+          options.hashtags.each do |tag|
+            throw :none unless content.downcase.include?("##{tag}")
+          end
         
-        maker.items.new_item do |item|
-          item.title = post.fetch("title")
+          maker.items.new_item do |item|
+            item.title = post.fetch("title")
 
-          # Elide title when text is very long
-          if item.title.length >= 40
-            item.title = item.title[0, 37]
-            item.title += '...'
-          end
-
-          item.link = post.fetch("url")
-          item.description = content
-          item.pubDate = post.fetch("updated")
-          item.author = post.fetch("actor").fetch("displayName")
-
-          if post.fetch("object").has_key?("attachments")
-            item.description += "<br /><br />"
-
-            attachments = post.fetch("object").fetch("attachments")
-            if attachments.first.has_key?("fullImage")
-              url = attachments.first.fetch("fullImage").fetch("url")
-              item.description += "<a href='#{url}'><img src='#{url}'></a>"
+            # Elide title when text is very long
+            if item.title.length >= 40
+              item.title = item.title[0, 37]
+              item.title += '...'
             end
+
+            item.link = post.fetch("url")
+            item.description = content
+            item.pubDate = post.fetch("updated")
+            item.author = post.fetch("actor").fetch("displayName")
+
+            if post.fetch("object").has_key?("attachments")
+              item.description += "<br /><br />"
+
+              attachments = post.fetch("object").fetch("attachments")
+              if attachments.first.has_key?("fullImage")
+                url = attachments.first.fetch("fullImage").fetch("url")
+                item.description += "<a href='#{url}'><img src='#{url}'></a>"
+              end
+            end
+
+            counter += 1
           end
         end
+
+        throw :done if counter >= options.limit
       end
     end
+    
+    puts rss
   end
-
-  puts rss
 else
   puts "Something went wrong :-("
 end
