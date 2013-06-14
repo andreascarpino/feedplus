cb9c42994ee96cb262fa23057f1cba37da82ec3b
Andrea Scarpino <scarpino@kde.org>
2013-06-14 14:07:48 +0200

    Move some code in dedicated functions

--- feedplus.rb
+++ feedplus.rb
@@ -21,113 +21,121 @@ require 'optparse'
 require 'ostruct'
 require 'rss'
 
-options = OpenStruct.new
-options.id = ""
-options.hashtags = []
-options.limit = 20
-options.feedTitle = "Your feed title"
-options.feedUrl = "http://your.domain.com/"
+$options = OpenStruct.new
+$options.id = ""
+$options.hashtags = []
+$options.limit = 20
+$options.feedTitle = "Your feed title"
+$options.feedUrl = "http://your.domain.com/"
 
-opt_parser = OptionParser.new do |opts|
-  opts.banner = "Usage: feedplus.rb [options]"
-  opts.separator ""
-  opts.separator "Specific options:"
+def getPosts(pageToken = "nextPageToken")
+  uri = URI("https://www.googleapis.com/plus/v1/people/#{$options.id}/activities/public")
+  params = { :fields => 'items(actor/displayName,object(actor/displayName,attachments(content,displayName,fullImage/url),content),title,published,updated,url),nextPageToken',
+             :key => 'AIzaSyDjcCZGSGTIaMA3VXmEjATkTlX4iRAoPiM',
+             :maxResults => 100,
+             :pageToken => pageToken }
+  uri.query = URI.encode_www_form(params)
 
-  opts.on("--id <id>", "--user <id>", "Google+ user ID") do |id|
-    options.id = id
-  end
-
-  opts.on("-f", "--filter tag1, tag2, ...", Array, "Only posts that have these hashtags") do |tags|
-    options.hashtags = tags.map{|t| t.downcase}
-  end
-
-  opts.on("-l", "--limit <n>", "Max N entries for feed") do |limit|
-    options.limit = limit
-  end
-
-  opts.on("-t", "--title <title>", "Feed title") do |title|
-    options.feedTitle = title
-  end
-  
-  opts.on("-u", "--url <url>", "Feed url") do |url|
-    options.feedUrl = url
-  end
-
-  opts.on_tail("-h", "--help", "Show this message") do
-    puts opts
+  res = Net::HTTP.get_response(uri)
+  if res.is_a?(Net::HTTPSuccess)
+    JSON.parse(res.body)
+  else
+    puts "Something went wrong :-("
     exit
   end
 end
 
-opt_parser.parse!(ARGV)
+def parse(args)
+  opt_parser = OptionParser.new do |opts|
+    opts.banner = "Usage: feedplus.rb [options]"
+    opts.separator ""
+    opts.separator "Specific options:"
 
-if options.id.empty?
+    opts.on("--id <id>", "--user <id>", "Google+ user ID") do |id|
+      $options.id = id
+    end
+
+    opts.on("-f", "--filter tag1, tag2, ...", Array, "Only posts that have these hashtags") do |tags|
+      $options.hashtags = tags.map{|t| t.downcase}
+    end
+
+    opts.on("-l", "--limit <n>", "Max N entries for feed. (Default to 20)") do |limit|
+      $options.limit = limit
+    end
+
+    opts.on("-t", "--title <title>", "Feed title") do |title|
+      $options.feedTitle = title
+    end
+  
+    opts.on("-u", "--url <url>", "Feed url") do |url|
+      $options.feedUrl = url
+    end
+
+    opts.on_tail("-h", "--help", "Show this message") do
+      puts opts
+      exit
+    end
+  end
+
+  opt_parser.parse!(args)
+end
+
+parse(ARGV)
+
+if $options.id.empty?
   puts "Please, specify an user id (see --help)."
   exit
 end
 
-uri = URI("https://www.googleapis.com/plus/v1/people/#{options.id}/activities/public")
-params = { :fields => 'items(actor/displayName,object(actor/displayName,attachments(content,displayName,fullImage/url),content),title,published,updated,url),nextPageToken',
-           :key => 'AIzaSyDjcCZGSGTIaMA3VXmEjATkTlX4iRAoPiM',
-           :maxResults => 100,
-           :pageToken => 'nextPageToken' }
-uri.query = URI.encode_www_form(params)
+rss = RSS::Maker.make("2.0") do |maker|
+  channel = maker.channel
+  channel.title = $options.feedTitle
+  channel.description = channel.title
+  channel.link = $options.feedUrl
 
-res = Net::HTTP.get_response(uri)
-if res.is_a?(Net::HTTPSuccess)
-  items = JSON.parse(res.body)
+  items = getPosts
+  counter = 0
 
-  rss = RSS::Maker.make("2.0") do |maker|
-    channel = maker.channel
-    channel.title = options.feedTitle
-    channel.description = channel.title
-    channel.link = options.feedUrl
+  catch :done do
+    items.fetch("items").each do |post|
+      content = post.fetch("object").fetch("content")
 
-    counter = 0
-
-    catch :done do
-      items.fetch("items").each do |post|
-        content = post.fetch("object").fetch("content")
-
-        catch :none do
-          options.hashtags.each do |tag|
-            throw :none unless content.downcase.include?("##{tag}")
-          end
-        
-          maker.items.new_item do |item|
-            item.title = post.fetch("title")
-
-            # Elide title when text is very long
-            if item.title.length >= 40
-              item.title = item.title[0, 37]
-              item.title += '...'
-            end
-
-            item.link = post.fetch("url")
-            item.description = content
-            item.pubDate = post.fetch("published")
-            item.author = post.fetch("actor").fetch("displayName")
-
-            if post.fetch("object").has_key?("attachments")
-              item.description += "<br /><br />"
-
-              attachments = post.fetch("object").fetch("attachments")
-              if attachments.first.has_key?("fullImage")
-                url = attachments.first.fetch("fullImage").fetch("url")
-                item.description += "<a href='#{url}'><img src='#{url}'></a>"
-              end
-            end
-
-            counter += 1
-          end
+      catch :none do
+        $options.hashtags.each do |tag|
+          throw :none unless content.downcase.include?("##{tag}")
         end
+        
+        maker.items.new_item do |item|
+          item.title = post.fetch("title")
 
-        throw :done if counter >= options.limit
+          # Elide title when text is very long
+          if item.title.length >= 40
+            item.title = item.title[0, 37]
+            item.title += '...'
+          end
+
+          item.link = post.fetch("url")
+          item.description = content
+          item.pubDate = post.fetch("published")
+          item.author = post.fetch("actor").fetch("displayName")
+
+          if post.fetch("object").has_key?("attachments")
+            item.description += "<br /><br />"
+
+            attachments = post.fetch("object").fetch("attachments")
+            if attachments.first.has_key?("fullImage")
+              url = attachments.first.fetch("fullImage").fetch("url")
+              item.description += "<a href='#{url}'><img src='#{url}'></a>"
+            end
+          end
+
+          counter += 1
+        end
       end
+
+      throw :done if counter >= $options.limit
     end
   end
-
-  puts rss
-else
-  puts "Something went wrong :-("
 end
+
+puts rss unless rss.nil?
